e6c060b7823405d63119d18c0c81ee392773703b
hl3470 <hl3470@naver.com>
2024-03-27 14:29:22 +0900

    feat: 웹 소켓 config, handler 구현

--- message/src/main/java/practice/message_project/MessageApplication.java
+++ message/src/main/java/practice/message_project/MessageApplication.java
@@ -1,4 +1,4 @@
-package practice.message;
+package practice.message_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- message/src/test/java/practice/message_project/MessageApplicationTests.java
+++ message/src/test/java/practice/message_project/MessageApplicationTests.java
@@ -1,4 +1,4 @@
-package practice.message;
+package practice.message_project;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
