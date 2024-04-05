040c53563f828237c150b455ef9e393303cb8b52
hl3470 <hl3470@naver.com>
2024-04-05 16:59:59 +0900

    feat: ui 개선
    채팅상대 클릭시 자동으로 채팅방 생성
    채팅방 클릭시 자동으로 세션 연결

--- message/src/main/resources/static/js/app.js
+++ message/src/main/resources/static/js/app.js
@@ -1,15 +1,12 @@
 
 let senderId = 1;
 let receiverId = 2;
-let chatRoomId = 1;
+let chatRoomId = 0;
 
 const stompClient = new StompJs.Client({
     brokerURL: 'ws://localhost:8080/ws-stomp'
 });
 
-window.onload = function () {
-    getChatRooms();
-};
 
 function connect() {
     stompClient.activate();
@@ -25,13 +22,12 @@ function disconnect() {
 stompClient.onConnect = (frame) => {
     setConnected(true);
     console.log('Connected: ' + frame);
-    for (let i = 0; i < memberChatRoomIds.length; i++) {
-        stompClient.subscribe('/sub/rooms/' + memberChatRoomIds[i], function(chat) {
-            console.log("connectMessageData: " + chat);
-            let response = JSON.parse(chat.body);
-            showMessage(response.data.senderNickName, response.data.message);
-        });
-    }
+    // loadChat()
+    stompClient.subscribe('/sub/chatRooms/' +  chatRoomId, function(chat) {
+        console.log("connectMessageData: " + chat);
+        let response = JSON.parse(chat.body);
+        showMessage(response.data.senderNickName, response.data.message);
+    });
 };
 
 
@@ -45,45 +41,41 @@ stompClient.onStompError = (frame) => {
 };
 
 
-// function connect() {
-//     var socket = new SockJS("/ws-stomp");
-//     stompClient = Stomp.over(socket);
-//     stompClient.connect({}, function (frame) {
-//         setConnected(true);
-//         console.log('Connected: ' + frame);
-//         loadChat(chatList)  //저장된 채팅 불러오기
-//
-//         //구독
-//         stompClient.subscribe('/room/'+chatRoomId, function (chatMessage) {
-//             showChat(JSON.parse(chatMessage.body));
-//         });
-//     });
-// }
-
 // 멤버 선택하기
 // memberId를 가져올 수 없어 임의로 만든 기능이니 추후 삭제 요망
 function switchMember(id) {
     senderId = id;
-    getChatRooms();
 }
 
 // 상대 멤버 선택하기
 // memberId를 가져올 수 없어 임의로 만든 기능이니 추후 삭제 요망
 function switchReceiver(id) {
     receiverId = id;
+
+    //사람을 클릭하면 해당하는 사람과의 채팅방이 생성 or 가져와진다.
+    makeRoom();
+    //채팅방 목록 갱신
+    getChatRooms();
 }
 
 // 채팅방 목록 가져오기
-let memberChatRoomIds = [];
+let memberChatRooms = [];
 function getChatRooms() {
+    let data = {
+        "pageNumber" : 0,
+        "pageSize" : 20
+    }
+
     $.ajax({
         type: 'get',
-        url: '/rooms/member/' + senderId,
+        url: '/api/chatRooms/members/' + senderId,
+        data: data,
         async: false,
         dataType: 'JSON',
         contentType : "application/json",
         success: function (data) {
-            memberChatRoomIds = data.data.roomIds;
+            memberChatRooms = data.data.content;
+            console.log(memberChatRooms);
         },
     });
     showRooms();
@@ -92,8 +84,8 @@ function getChatRooms() {
 // 채팅방 목록 띄우기
 function showRooms() {
     $("#room-group").empty();
-    for (let i = 0; i < memberChatRoomIds.length; i++) {
-        let text = '<button class="btn btn-primary" type="submit" onclick="selectRoom(' + memberChatRoomIds[i] + ')">' + memberChatRoomIds[i] + '번 채팅방</button>';
+    for (let i = 0; i < memberChatRooms.length; i++) {
+        let text = '<button class="btn btn-primary" type="submit" onclick="selectRoom(' + memberChatRooms[i].chatRoomId + ')">' + memberChatRooms[i].roomName + ' 채팅방</button>';
         $("#room-group").append(text);
     }
 
@@ -104,6 +96,8 @@ function showRooms() {
 // 방 들어가기
 function selectRoom(roomId) {
     chatRoomId = roomId;
+    //방연결
+    connect();
 }
 
 function setConnected(connected) {
@@ -129,8 +123,8 @@ function makeRoom() {
 
     $.ajax({
         type: 'post',
-        url: '/room',
-        async: true,
+        url: 'api/chatRooms',
+        async: false,
         data: JSON.stringify(data),
         dataType: 'JSON',
         contentType : "application/json",
@@ -147,8 +141,13 @@ function sendTalkMessage() {
     console.log("senderId: " + senderId);
     console.log("receiverId: " + receiverId);
 
+    if (chatRoomId === 0) {
+        alert("채팅방을 선택해주세요");
+        return;
+    }
+
     stompClient.publish({
-        destination: "/pub/" + chatRoomId,
+        destination: "/pub/chatRooms/" + chatRoomId,
         body: JSON.stringify({'messageType': "TALK", 'message': $("#message").val(), "chatRoomId" : chatRoomId, "senderId" : senderId, "receiverId" : receiverId})
     });
 }
@@ -156,7 +155,7 @@ function sendTalkMessage() {
 //나가기
 function sendOutMessage() {
     stompClient.publish({
-        destination: "/pub/" + chatRoomId,
+        destination: "/pub/chatRooms/" + chatRoomId,
         body: JSON.stringify({'messageType': "OUT", "chatRoomId" : chatRoomId, "senderId" : senderId, "receiverId" : receiverId})
     });
 }
@@ -201,7 +200,7 @@ function getChats() {
 
     $.ajax({
         type: 'post',
-        url: '/room',
+        url: '/api/chatRooms',
         async: true,
         data: JSON.stringify(data),
         dataType: 'JSON',
